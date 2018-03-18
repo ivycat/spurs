38772dd67f631887a55e3e25aec655c098f8d24f
Eric Amundson <eric@ivycat.com>
2018-03-18 16:05:48 -0700

    Remove comments from pages.

--- page.php
+++ page.php
@@ -15,9 +15,4 @@ endif;
 
 while ( have_posts() ) : the_post();
 	get_template_part( 'loop-templates/content', 'page' );
-
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
 endwhile; // end of the loop.
