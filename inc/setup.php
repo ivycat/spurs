88c6b1de58ef922cfd09523db7126cbf1f8ffc78
Eric Amundson <eric@ivycat.com>
2018-08-12 12:21:48 -0500

    WooCommerce updates, code cleanup

--- inc/setup.php
+++ inc/setup.php
@@ -24,7 +24,7 @@ if ( ! function_exists( 'spurs_setup' ) ) {
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on understrap, use a find and replace
+		 * If you're building a theme based on Spurs, use a find and replace
 		 * to change 'spurs' to the name of your theme in all the template files
 		 */
 		load_theme_textdomain( 'spurs', get_template_directory() . '/languages' );
