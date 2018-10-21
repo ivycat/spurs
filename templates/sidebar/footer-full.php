99d3ac068855f348df842190de8f6653babaa5bb
Eric Amundson <eric@ivycat.com>
2018-10-20 19:45:15 -0700

    move templates

--- templates/sidebar/footer-full.php
+++ templates/sidebar/footer-full.php
@@ -12,12 +12,12 @@ $container = get_theme_mod( 'spurs_container_type' );
  */
 if ( is_active_sidebar( 'footer-full' ) ) { ?>
 
-	<div class="wrapper" id="wrapper-footer-full">
-		<div class="<?php echo esc_attr( $container ); ?>" id="footer-full-content" tabindex="-1">
-			<div class="row">
+    <div class="wrapper" id="wrapper-footer-full">
+        <div class="<?php echo esc_attr( $container ); ?>" id="footer-full-content" tabindex="-1">
+            <div class="row">
 				<?php dynamic_sidebar( 'footer-full' ); ?>
-			</div>
-		</div>
-	</div><!-- #wrapper-footer-full -->
+            </div>
+        </div>
+    </div><!-- #wrapper-footer-full -->
 
 <?php } ?>
