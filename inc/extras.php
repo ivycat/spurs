68cb954548acb2ac27c7481e96fce209b168544f
Eric Amundson <eric@ivycat.com>
2018-10-23 10:41:08 -0700

    Code cleanup

--- inc/extras.php
+++ inc/extras.php
@@ -219,43 +219,43 @@ remove_action( 'wp_head', 'wp_generator' );
 /**
  * Remove WooCommerce Generator HTML comments.
  */
-remove_action( 'wp_head', 'wc_generator_tag' ); // WooCommerce
+remove_action( 'wp_head', 'wc_generator_tag' );
+
 /**
  * Remove All in One SEO Pack HTML comments
  * @link //gist.github.com/llgruff/a7ab776167aa0ed307ec445df54e5fdb
  */
-if (defined('AIOSEOP_VERSION')) {
-	add_action('get_header', function() {
+if ( defined( 'AIOSEOP_VERSION' ) ) {
+	add_action( 'get_header', function () {
 		ob_start(
-			function($o) {
-				return preg_replace('/\n?<.*?One SEO Pack.*?>/mi','',$o);
+			function ( $o ) {
+				return preg_replace( '/\n?<.*?One SEO Pack.*?>/mi', '', $o );
 			}
 		);
-	});
-	add_action('wp_head', function() {
+	} );
+	add_action( 'wp_head', function () {
 		ob_end_flush();
-	}, 999);
+	}, 999 );
 }
 /**
  * Remove Yoast SEO HTML comments
  * @link //gist.github.com/llgruff/a7ab776167aa0ed307ec445df54e5fdb
  * @link //gist.github.com/paulcollett/4c81c4f6eb85334ba076
  */
-if (defined('WPSEO_VERSION')) {
-	add_action('get_header', function() {
+if ( defined( 'WPSEO_VERSION' ) ) {
+	add_action( 'get_header', function () {
 		ob_start(
-			function($o) {
-				return preg_replace('/\n?<.*?yoast.*?>/mi','',$o);
+			function ( $o ) {
+				return preg_replace( '/\n?<.*?yoast.*?>/mi', '', $o );
 			}
 		);
-	});
-	add_action('wp_head', function() {
+	} );
+	add_action( 'wp_head', function () {
 		ob_end_flush();
-	}, 999);
+	}, 999 );
 }
 /**
  * Remove Google Analytics by MonsterInsights HTML comments
- *
  * @link https://wordpress.org/plugins/remove-google-analytics-comments/
  */
 function isgabmi_active( $plugin ) {
