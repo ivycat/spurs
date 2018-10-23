0ec8f0fef6aa71045a6ae6a55ae2886875a2653a
Eric Amundson <eric@ivycat.com>
2018-10-23 10:14:17 -0700

    Code cleanup functions for Yoast and MonsterInsights

--- inc/extras.php
+++ inc/extras.php
@@ -212,9 +212,71 @@ function return_template( $file, $args = null, $default_folder = 'parts' ) {
 }
 
 /**
- * Code cleanup
- * Remove WordPress and WooCommerce Generator tags from HTML markup.
+ * Remove WordPress Generator HTML comments
  */
-remove_action( 'wp_head', 'wp_generator' );     // WordPress
+remove_action( 'wp_head', 'wp_generator' );
+
+/**
+ * Remove WooCommerce Generator HTML comments.
+ */
 remove_action( 'wp_head', 'wc_generator_tag' ); // WooCommerce
-    
+/**
+ * Remove All in One SEO Pack HTML comments
+ * @link //gist.github.com/llgruff/a7ab776167aa0ed307ec445df54e5fdb
+ */
+if (defined('AIOSEOP_VERSION')) {
+	add_action('get_header', function() {
+		ob_start(
+			function($o) {
+				return preg_replace('/\n?<.*?One SEO Pack.*?>/mi','',$o);
+			}
+		);
+	});
+	add_action('wp_head', function() {
+		ob_end_flush();
+	}, 999);
+}
+/**
+ * Remove Yoast SEO HTML comments
+ * @link //gist.github.com/llgruff/a7ab776167aa0ed307ec445df54e5fdb
+ * @link //gist.github.com/paulcollett/4c81c4f6eb85334ba076
+ */
+if (defined('WPSEO_VERSION')) {
+	add_action('get_header', function() {
+		ob_start(
+			function($o) {
+				return preg_replace('/\n?<.*?yoast.*?>/mi','',$o);
+			}
+		);
+	});
+	add_action('wp_head', function() {
+		ob_end_flush();
+	}, 999);
+}
+/**
+ * Remove Google Analytics by MonsterInsights HTML comments
+ *
+ * @link https://wordpress.org/plugins/remove-google-analytics-comments/
+ */
+function isgabmi_active( $plugin ) {
+	$network_active = false;
+	if ( is_multisite() ) {
+		$plugins = get_site_option( 'active_sitewide_plugins' );
+		if ( isset( $plugins[ $plugin ] ) ) {
+			$network_active = true;
+		}
+	}
+
+	return in_array( $plugin, get_option( 'active_plugins' ) ) || $network_active;
+}
+
+if ( isgabmi_active( 'google-analytics-for-wordpress/googleanalytics.php' ) || isgabmi_active( 'google-analytics-premium/googleanalytics.php' ) ) {
+	add_action( 'get_header', function () {
+		ob_start( function ( $o ) {
+			return preg_replace( '/\n?<.*?monsterinsights.*?>/mi', '', $o );
+		} );
+	} );
+	add_action( 'wp_head', function () {
+		ob_end_flush();
+	}, 999 );
+}
