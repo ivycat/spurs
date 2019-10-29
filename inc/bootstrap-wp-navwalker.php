a95fb7665280ef07b5a96b880f17d25126209acf
Eric Amundson <eric@ivycat.com>
2019-10-29 11:07:28 -0500

    persnickety formatting

--- inc/bootstrap-wp-navwalker.php
+++ inc/bootstrap-wp-navwalker.php
@@ -349,6 +349,7 @@ if ( ! class_exists( 'Spurs_WP_Bootstrap_Navwalker' ) ) {
 		 * and will add a link to the WordPress menu manager if logged in as an admin.
 		 *
 		 * @param array $args passed from the wp_nav_menu function.
+		 *
 		 * @return null|string
 		 */
 		public static function fallback( $args ) {
