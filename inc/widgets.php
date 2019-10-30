97f497d80954edb6ad5bbd72aa3370fc2e1a3940
Eric Amundson <eric@ivycat.com>
2019-10-30 18:01:37 -0500

    Update widget logic

--- inc/widgets.php
+++ inc/widgets.php
@@ -9,44 +9,84 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Count number of widgets in a sidebar
- * Used to add classes to widget areas so widgets can be displayed one, two, three or four per row
+ * Add filter to the parameters passed to a widget's display callback.
+ * The filter is evaluated on both the front and the back end!
+ *
+ * @link https://developer.wordpress.org/reference/hooks/dynamic_sidebar_params/
  */
-if ( ! function_exists( 'spurs_count_widgets' ) ) {
-	function spurs_count_widgets( $sidebar_id ) {
-		// If loading from front page, consult $_wp_sidebars_widgets rather than options
-		// to see if wp_convert_widget_settings() has made manipulations in memory.
-		global $_wp_sidebars_widgets;
-		if ( empty( $_wp_sidebars_widgets ) ) {
-			$_wp_sidebars_widgets = get_option( 'sidebars_widgets', array() );
-		}
-		$sidebar_widget_count = $_wp_sidebars_widgets;
+add_filter( 'dynamic_sidebar_params', 'spurs_widget_classes' );
 
-		if ( isset( $sidebar_widget_count[ $sidebar_id ] ) ) {
-			$widget_count   = count( $sidebar_widget_count[ $sidebar_id ] );
-			$widget_classes = 'widget-count-' . count( $sidebar_widget_count[ $sidebar_id ] );
+if ( ! function_exists( 'spurs_widget_classes' ) ) {
+	/**
+	 * Count number of visible widgets in a sidebar and add classes to widgets accordingly,
+	 * so widgets can be displayed one, two, three or four per row.
+	 *
+	 * @global array $sidebars_widgets
+	 *
+	 * @param array $params {
+	 *     @type array $args  {
+	 *         An array of widget display arguments.
+	 *
+	 *         @type string $name          Name of the sidebar the widget is assigned to.
+	 *         @type string $id            ID of the sidebar the widget is assigned to.
+	 *         @type string $description   The sidebar description.
+	 *         @type string $class         CSS class applied to the sidebar container.
+	 *         @type string $before_widget HTML markup to prepend to each widget in the sidebar.
+	 *         @type string $after_widget  HTML markup to append to each widget in the sidebar.
+	 *         @type string $before_title  HTML markup to prepend to the widget title when displayed.
+	 *         @type string $after_title   HTML markup to append to the widget title when displayed.
+	 *         @type string $widget_id     ID of the widget.
+	 *         @type string $widget_name   Name of the widget.
+	 *     }
+	 *     @type array $widget_args {
+	 *         An array of multi-widget arguments.
+	 *
+	 *         @type int $number Number increment used for multiples of the same widget.
+	 *     }
+	 * }
+	 * @return array $params
+	 */
+	function spurs_widget_classes( $params ) {
 
-			if ( 0 == $widget_count % 4 || $widget_count > 6 ) {
-				// Four widgets per row if there are exactly four or more than six
+		global $sidebars_widgets;
+
+		/*
+		 * When the corresponding filter is evaluated on the front end
+		 * this takes into account that there might have been made other changes.
+		 */
+		$sidebars_widgets_count = apply_filters( 'sidebars_widgets', $sidebars_widgets );
+
+		// Only apply changes if sidebar ID is set and the widget's classes depend on the number of widgets in the sidebar.
+		if ( isset( $params[0]['id'] ) && strpos( $params[0]['before_widget'], 'dynamic-classes' ) ) {
+			$sidebar_id   = $params[0]['id'];
+			$widget_count = count( $sidebars_widgets_count[ $sidebar_id ] );
+
+			$widget_classes = 'widget-count-' . $widget_count;
+			if ( 0 === $widget_count % 4 || $widget_count > 6 ) {
+				// Four widgets per row if there are exactly four or more than six.
 				$widget_classes .= ' col-md-3';
-			} elseif ( 6 == $widget_count ) {
-				// If two widgets are published
+			} elseif ( 6 === $widget_count ) {
+				// If two widgets are published.
 				$widget_classes .= ' col-md-2';
 			} elseif ( $widget_count >= 3 ) {
-				// Three widgets per row if there's three or more widgets
+				// Three widgets per row if there's three or more widgets.
 				$widget_classes .= ' col-md-4';
-			} elseif ( 2 == $widget_count ) {
-				// If two widgets are published
+			} elseif ( 2 === $widget_count ) {
+				// If two widgets are published.
 				$widget_classes .= ' col-md-6';
-			} elseif ( 1 == $widget_count ) {
-				// If just on widget is active
+			} elseif ( 1 === $widget_count ) {
+				// If just on widget is active.
 				$widget_classes .= ' col-md-12';
 			}
 
-			return $widget_classes;
+			// Replace the placeholder class 'dynamic-classes' with the classes stored in $widget_classes.
+			$params[0]['before_widget'] = str_replace( 'dynamic-classes', $widget_classes, $params[0]['before_widget'] );
 		}
+
+		return $params;
+
 	}
-}
+} // endif function_exists( 'spurs_widget_classes' ).
 
 add_action( 'widgets_init', 'spurs_widgets_init' );
 if ( ! function_exists( 'spurs_widgets_init' ) ) {
@@ -98,7 +138,7 @@ if ( ! function_exists( 'spurs_widgets_init' ) ) {
 			'name'          => __( 'Top Full', 'spurs' ),
 			'id'            => 'hero-static',
 			'description'   => __( 'Full top widget with dynamic grid', 'spurs' ),
-			'before_widget' => '<div id="%1$s" class="hero-static-widget %2$s ' . spurs_count_widgets( 'hero-static' ) . '">',
+			'before_widget' => '<div id="%1$s" class="hero-static-widget %2$s dynamic-classes">',
 			'after_widget'  => '</div>',
 			'before_title'  => '<h3 class="widget-title">',
 			'after_title'   => '</h3>',
@@ -108,7 +148,7 @@ if ( ! function_exists( 'spurs_widgets_init' ) ) {
 			'name'          => __( 'Footer Full', 'spurs' ),
 			'id'            => 'footer-full',
 			'description'   => 'Widget area below main content and above footer',
-			'before_widget' => '<div id="%1$s" class="footer-widget %2$s ' . spurs_count_widgets( 'footer-full' ) . '">',
+			'before_widget' => '<div id="%1$s" class="footer-widget %2$s dynamic-classes">',
 			'after_widget'  => '</div>',
 			'before_title'  => '<h3 class="widget-title">',
 			'after_title'   => '</h3>',
