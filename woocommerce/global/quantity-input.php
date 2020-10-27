216a7cdd9d2fee6813a3ec2a9bbb66f8abeb36ce
priyank <priyank.patel@multidots.com>
2020-10-27 18:41:41 +0530

    woocommerce template changes.

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.6.1
+ * @version 4.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -20,20 +20,20 @@ defined( 'ABSPATH' ) || exit;
 if ( $max_value && $min_value === $max_value ) {
 	?>
 	<div class="quantity hidden">
-		<input type="hidden" id="<?php echo esc_attr( $input_id ); ?>" class="qty"
-		       name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $min_value ); ?>"/>
+		<input type="hidden" id="<?php echo esc_attr( $input_id ); ?>" class="qty" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $min_value ); ?>"/>
 	</div>
 	<?php
 } else {
 	/* translators: %s: Quantity. */
-	$label = ! empty( $args['product_name'] ) ? sprintf( __( '%s quantity', 'spurs' ), wp_strip_all_tags( $args['product_name'] ) ) : __( 'Quantity', 'spurs' );
+	$label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'spurs' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'spurs' );
 	?>
 	<div class="quantity">
+		<?php do_action( 'woocommerce_before_quantity_input_field' ); ?>
 		<label class="sr-only" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
 		<input
 			type="number"
 			id="<?php echo esc_attr( $input_id ); ?>"
-			class="input-text qty text"
+			class="input-text qty text <?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
 			step="<?php echo esc_attr( $step ); ?>"
 			min="<?php echo esc_attr( $min_value ); ?>"
 			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
@@ -41,7 +41,9 @@ if ( $max_value && $min_value === $max_value ) {
 			value="<?php echo esc_attr( $input_value ); ?>"
 			title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'spurs' ); ?>"
 			size="4"
+			placeholder="<?php echo esc_attr( $placeholder ); ?>"
 			inputmode="<?php echo esc_attr( $inputmode ); ?>"/>
+		<?php do_action( 'woocommerce_after_quantity_input_field' ); ?>
 	</div>
 	<?php
 }
